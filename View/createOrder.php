199d0c3689193c048d9dca07a7b48d1bf93b9dbc
Arcangel <ahiguerl@gmail.com>
2018-04-12 21:45:56 -0500

    crearOrden

---
+++ View/createOrder.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Órden de Servicio</title>
+</head>
+<body>
+
+  <h1>CREACIÓN  DE ÓRDENES</h1>
+
+  <form class="" action="createOrder.php" method="post">
+    Id: <input type="number" name="id" value=""><br/><br/>
+    Id Cliente: <input type="number" name="idCliente" value=""><br/><br/>
+    Fecha/Hora: <input type="number" name="fechaHora" value=""><br/><br/>
+    Descripcion: <input type="number" name="descripcion" value=""><br/><br/>
+    Cantidad Productos: <input type="number" name="cantidadProductos" value=""><br/><br/>
+    Costo Total: <input type="number" name="costoTotal" value=""><br/><br/>
+    <input type="submit" name="Guardar!">
+  </form>
+
+
+
+
+</body>
+</html>
