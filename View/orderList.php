14c410f35b7cb55f4af833fe9ac35ab6785f292b
karlospuntocom <cadasago_533@hotmail.com>
2018-04-12 20:03:08 -0500

    C2A

--- View/orderList.php
+++ View/orderList.php
@@ -8,7 +8,7 @@
 		<tr><th>Id</th><th>Id Cliente</th><th>Fecha/Hora</th><th>Descripción</th><th>Cantidad Productos</th><th>Costo Total</th></tr>
 		<?<?php
 			foreach ($orders as $title => $order) {
-				echo '<tr><td><a href="index.php?book='.$order->id.'">'.$order->id.'</a></td><td>'.$order->idCliente.'</td><td>'.$order->fechaHora.'</td><td>'.$order->descripcion.'</td><td>'.$order->cantidadProductos.'</td><td>'.$order->costoTotal.'</td></tr>';
+				echo '<tr><td><a href="index.php?order='.$order->id.'">'.$order->id.'</a></td><td>'.$order->idCliente.'</td><td>'.$order->fechaHora.'</td><td>'.$order->descripcion.'</td><td>'.$order->cantidadProductos.'</td><td>'.$order->costoTotal.'</td></tr>';
 			}
 		?>
 	</table>
