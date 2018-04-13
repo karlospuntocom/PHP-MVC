6ac6b995aeed964a7ce2010817a5e8a4b33c98c1
karlospuntocom <cadasago_533@hotmail.com>
2018-04-12 19:59:30 -0500

    CA2

--- View/orderList.php
+++ View/orderList.php
@@ -7,8 +7,8 @@
 	<table>
 		<tr><th>Id</th><th>Id Cliente</th><th>Fecha/Hora</th><th>Descripción</th><th>Cantidad Productos</th><th>Costo Total</th></tr>
 		<?<?php
-			foreach ($notifs as $title => $notif) {
-				echo '<tr><td><a href="index.php?book='.$notif->id.'">'.$notif->id.'</a></td><td>'.$notif->idCliente.'</td><td>'.$notif->fechaHora.'</td><td>'.$notif->descripcion.'</td><td>'.$notif->cantidadProductos.'</td><td>'.$notif->costoTotal.'</td></tr>';
+			foreach ($orders as $title => $order) {
+				echo '<tr><td><a href="index.php?book='.$order->id.'">'.$order->id.'</a></td><td>'.$order->idCliente.'</td><td>'.$order->fechaHora.'</td><td>'.$order->descripcion.'</td><td>'.$order->cantidadProductos.'</td><td>'.$order->costoTotal.'</td></tr>';
 			}
 		?>
 	</table>
